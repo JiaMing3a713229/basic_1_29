54ec3f2d16eb281b95a052e7f14e0039294b7f85
JiaMing <ty10410205@gmail.com>
2019-03-30 15:55:32 +0800

    change 0

---
+++ basic_1.cpp
@@ -0,0 +1,9 @@
+//四子一丙 3a713229 作業01 
+#include <stdio.h>                //載入標頭檔 
+#include <stdlib.h> 
+int main(void)
+{	                        
+ printf("Hello World 3a713229\n");  //螢幕顯示"Hello World 3a713229 
+ system("pause");                   //暫停，案任意鍵繼續 
+ return 0;                          //回傳0 
+}
