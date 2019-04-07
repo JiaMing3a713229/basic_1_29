7b39a808f831c76e1e911afca47a7b75f6f0f88a
JiaMing <ty10410205@gmail.com>
2019-04-07 17:48:43 +0800

    add text

--- basic_1.cpp
+++ basic_1.cpp
@@ -1,6 +1,11 @@
-//四子一丙 3a713229 作業01 
+/* 
+題目名稱:請輸出hello world + 學號
+製作:呂家明 
+修改/完成日期:2019/4/7 
+*/ 
 #include <stdio.h>                //載入標頭檔 
 #include <stdlib.h> 
+
 int main(void)
 {	                        
  printf("Hello World 3a713229\n");  //螢幕顯示"Hello World 3a713229 
